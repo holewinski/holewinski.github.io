4a59848a7260484d98b709446c409375c6c13f82
holewinski <szymon.holewinski@gmail.com>
2020-09-03 19:49:55 +0200

    Initial dist subtree commit

---
+++ task-roulette/dist/precache-manifest.a61af3582d49b95dbdd7b579b7f382a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8a6e5d35b6080a180a",
+    "url": "/css/app.e59417a8.css"
+  },
+  {
+    "revision": "6922433288e4e4f845a6d4d65eb0d6be",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "ff105873af5b514d6d500b0565383b1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a8a6e5d35b6080a180a",
+    "url": "/js/app.7120c60b.js"
+  },
+  {
+    "revision": "5b35f9bc07e7b37ce16e",
+    "url": "/js/chunk-vendors.bd6bd562.js"
+  },
+  {
+    "revision": "2e129689b08481e044a1c7f033b6d569",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "86014a3a3d24cc282950e56f844e6ec7",
+    "url": "/service-worker.js"
+  }
+]);
---
+++ task-roulette/dist/service-worker.js
@@ -0,0 +1,14 @@
+importScripts("/precache-manifest.a61af3582d49b95dbdd7b579b7f382a1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+if (workbox) {
+    console.log(`Workbox is loaded`);
+
+    workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+}
+else {
+    console.log(`Workbox didn't load`);
+}
+
+
+
