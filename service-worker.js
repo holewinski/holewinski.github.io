4f1fe6658cb42366b67720118f5f3f9bb6c6a59a
holewinski <szymon.holewinski@gmail.com>
2020-09-03 20:23:54 +0200

    Initial dist subtree commit

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+importScripts("/precache-manifest.77bb91593b3349c095a88abe57cf160d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+if (workbox) {
+    console.log(`Workbox is loaded`);
+
+    workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+}
+else {
+    console.log(`Workbox didn't load`);
+}
+
+
+
