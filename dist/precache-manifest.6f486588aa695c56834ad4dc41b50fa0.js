5b8471cf02dda4cbc3b0d89cb29cbdad0acd2896
holewinski <szymon.holewinski@gmail.com>
2020-09-03 23:07:44 +0200

    For github page

---
+++ dist/precache-manifest.6f486588aa695c56834ad4dc41b50fa0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966b8827ccd6c9c0acc7",
+    "url": "/css/app.e7182a8b.css"
+  },
+  {
+    "revision": "6922433288e4e4f845a6d4d65eb0d6be",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "333cc8760b470ed85f0097dda09bdc07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "966b8827ccd6c9c0acc7",
+    "url": "/js/app.1a54f5ce.js"
+  },
+  {
+    "revision": "9c508eb2cf7bf97f9d34",
+    "url": "/js/chunk-vendors.8c40973a.js"
+  },
+  {
+    "revision": "2e129689b08481e044a1c7f033b6d569",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5fc1600fa62e56bcd9c1b412b68180ea",
+    "url": "/service-worker.js"
+  }
+]);
