fb3505eac1f88ed9cc76416e4cf07149a3b11a51
holewinski <szymon.holewinski@gmail.com>
2020-09-03 19:38:26 +0200

    Working Roulette PWA

--- task-roulette/vue.config.js
+++ task-roulette/vue.config.js
@@ -12,38 +12,63 @@ module.exports = {
             short_name: 'TRoulette',
             orientation: 'portrait',
             display: 'standalone',
-            icons: [
-                {
-                    "src": "img/icons/icon-16.png",
-                    "sizes": "16x16",
-                    "type": "image/png"
-                }, {
-                    "src": "img/icons/icon-32.png",
-                    "sizes": "32x32",
-                    "type": "image/png"
-                }, {
-                    "src": "img/icons/icon-144.png",
-                    "sizes": "144x144",
-                    "type": "image/png"
-                }, {
-                    "src": "img/icons/icon-152.png",
-                    "sizes": "152x152",
-                    "type": "image/png"
-                },
-                {
-                    "src": "img/icons/icon-192.png",
-                    "sizes": "192x192",
-                    "type": "image/png"
-                },
-                {
-                    "src": "img/icons/icon-512.png",
-                    "sizes": "512x512",
-                    "type": "image/png"
-                }]
+            icons: [{
+                "src": "img/icons/icon-16-maskable.png",
+                "sizes": "16x16",
+                "type": "image/png",
+                "purpose": "maskable"
+            }, {
+                "src": "img/icons/icon-32-maskable.png",
+                "sizes": "32x32",
+                "type": "image/png",
+                "purpose": "maskable"
+            }, {
+                "src": "img/icons/icon-72-maskable.png",
+                "sizes": "72x72",
+                "type": "image/png",
+                "purpose": "maskable"
+            }, {
+                "src": "img/icons/icon-144-maskable.png",
+                "sizes": "144x144",
+                "type": "image/png",
+                "purpose": "maskable"
+            },
+            {
+                "src": "img/icons/icon-16.png",
+                "sizes": "16x16",
+                "type": "image/png"
+            }, {
+                "src": "img/icons/icon-32.png",
+                "sizes": "32x32",
+                "type": "image/png"
+            }, {
+                "src": "img/icons/icon-144.png",
+                "sizes": "144x144",
+                "type": "image/png"
+            }, {
+                "src": "img/icons/icon-152.png",
+                "sizes": "152x152",
+                "type": "image/png"
+            },
+            {
+                "src": "img/icons/icon-192.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "img/icons/icon-512.png",
+                "sizes": "512x512",
+                "type": "image/png"
+            }]
         },
         // configure the workbox plugin
 
-        workboxPluginMode: 'GenerateSW',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'public/service-worker.js',
+            // ...other Workbox options...
+        }
 
     }
 }
