2c1f046ade69f80b0b7e1aac7f986d5b0702c4ba
holewinski <szymon.holewinski@gmail.com>
2020-10-27 16:16:09 +0100

    Errors cath and small improvments

---
+++ dist/precache-manifest.803ec1687b006ad44d4dd7606b1c0102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55d54e12b5a7cf5a4b0",
+    "url": "/css/app.532923a1.css"
+  },
+  {
+    "revision": "3c513debc22b79ffc2f483f3a05175f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55d54e12b5a7cf5a4b0",
+    "url": "/js/app.e2608c7e.js"
+  },
+  {
+    "revision": "85737d4e6ba82814b0b9",
+    "url": "/js/chunk-vendors.1aea4345.js"
+  },
+  {
+    "revision": "2e129689b08481e044a1c7f033b6d569",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5fc1600fa62e56bcd9c1b412b68180ea",
+    "url": "/service-worker.js"
+  }
+]);
