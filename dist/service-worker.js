1d12dfebd9081066239abc755caf49a0e3633a46
holewinski <szymon.holewinski@gmail.com>
2020-10-27 14:56:32 +0100

    Errors catch and small improvments

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.eb2563d6842ef44ac0160e8909a712a2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.fe2b87288e1e4fd1e43d30bbadb24c30.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 if (workbox) {
     console.log(`Workbox is loaded`);
